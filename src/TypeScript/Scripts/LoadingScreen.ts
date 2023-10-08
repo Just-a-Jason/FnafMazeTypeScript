cfeefd545881cc86dc1ec0d07a91be1680a593a6
Jason.json <88512392+Just-a-Jason@users.noreply.github.com>
2023-10-08 21:07:52 +0200

    Loading screen added

---
+++ src/TypeScript/Scripts/LoadingScreen.ts
@@ -0,0 +1,46 @@
+const SPINER_PATH:string = '../Images/Assets/spinner.gif';
+const BANER_PATH:string = '../Images/Assets/baner.webp';
+
+let LoadingScreen_Instance:Nullable<LoadingScreen> = null;
+
+function Init():void {
+    const spinner:HTMLImageElement = document.createElement('img');
+    const baner: HTMLImageElement = document.createElement('img');
+    const ls:HTMLDivElement = document.createElement('div');
+
+    baner.classList.add("loading-baner");
+    baner.src = BANER_PATH;
+    baner.alt = 'baner';
+
+
+    spinner.alt = 'loading spinner';
+    spinner.src = SPINER_PATH;
+    spinner.classList.add('loading-spinner');
+
+    const loadingText:HTMLParagraphElement = document.createElement('p');
+    loadingText.innerText = 'Loading..';
+    ls.appendChild(loadingText);
+
+    ls.classList.add("loadingScreen");
+    ls.appendChild(spinner);
+    ls.appendChild(baner);
+
+    document.body.appendChild(ls);
+    LoadingScreen_Instance = ls;
+
+    document.addEventListener('DOMContentLoaded', (e:Event) => {
+        console.info('Page loaded!');
+        setTimeout(() => {
+            DoneLoading();
+        }, 200);
+    });
+}
+
+function DoneLoading():void {
+    LoadingScreen_Instance!.style["transform"] = "translateY(-1000px)";
+    setTimeout(() => {
+        LoadingScreen_Instance?.remove();
+    }, 500);
+}
+
+Init();
--- src/TypeScript/Types.ts
+++ src/TypeScript/Types.ts
@@ -2,3 +2,4 @@ type Nullable<T> = T | null;
 type UISelectableButton = HTMLDivElement;
 type UICategoryButton = HTMLDivElement;
 type CategoryList = HTMLDivElement;
+type LoadingScreen = HTMLDivElement;
